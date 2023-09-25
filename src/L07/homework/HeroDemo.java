3f554e90ce52465d8bd8dea2018d3e6f5e8b6e6d
Dacian Cioc <dacian.cioc@gmail.com>
2023-09-25 11:34:15 +0300

    updated based on comments

--- src/L07/homework/HeroDemo.java
+++ src/L07/homework/HeroDemo.java
@@ -18,26 +18,26 @@ public class HeroDemo {
 
 
         // Display hero information
-        System.out.println("Hero 1 Information:");
-        System.out.println("Name: " + Heroes.getname());
-        System.out.println("Health: " + Heroes.getHealth());
-        System.out.println("Experience: " + Heroes.getExperience());
-        System.out.println("Strength: " + Heroes.getStrength());
-        System.out.println("Level: " + Heroes.getLevel());
+//        System.out.println("Hero 1 Information:");
+//        System.out.println("Name: " + Heroes.getname());
+//        System.out.println("Health: " + Heroes.getHealth());
+//        System.out.println("Experience: " + Heroes.getExperience());
+//        System.out.println("Strength: " + Heroes.getStrength());
+//        System.out.println("Level: " + Heroes.getLevel());
 
         // Task 4: Battle with 10 iterations
         while (interactions < 10){
             int heroLevel = new Random().nextInt(4) + 4;
-            System.out.println("-------------------------------");
-            System.out.println("heroLevel " + heroLevel);
+//            System.out.println("-------------------------------");
+//            System.out.println("Hero Level: " + heroLevel);
             int enemyHealth = random.nextInt((101) + 100) * heroLevel; // Random enemy health between 100 and 200
-            System.out.println("enemy Health " + enemyHealth);
+//            System.out.println("Enemy Health: " + enemyHealth);
 
             int enemyStrength = random.nextInt(3) + 8 + (int) (Heroes.getStrength() * 0.10); // Random enemy strength
-            System.out.println("Strength Enemy " + enemyStrength);
+//            System.out.println("Strength Enemy: " + enemyStrength);
 
             int enemyLevel = random.nextInt(Heroes.getLevel()) + 1; // Random enemy level
-            System.out.println("Enemy Level " + enemyLevel);
+//            System.out.println("Enemy Level: " + enemyLevel);
 
 
             Enemies enemy = new Enemies(enemyHealth, enemyStrength, enemyLevel);
--- src/L07/homework/Heroes.java
+++ src/L07/homework/Heroes.java
@@ -49,26 +49,79 @@ public class Heroes {
         return level;
     }
 
-    public static void attack(@NotNull Enemies enemy) {
-        boolean isCriticalHit = Math.random() <= 0.1;
-        int damage = strength + (isCriticalHit ? 3 : 0);
-        enemy.health -= damage;
+    public static void attack(Enemies enemy) {
+        Random random = new Random();
+        boolean heroStarts = random.nextBoolean(); // Toss a coin to determine who starts
 
-        if (enemy.health <= 0) {
-            enemy.health = 0;
-            System.out.println("Hero defeated the enemy! ");
+        while (health > 0 && enemy.getHealth() > 0) {
+            if (heroStarts) {
+                // Hero's turn to attack
+                int heroDamage = calculateDamage();
+                if (random.nextDouble() <= 0.3) {
+                    // Critical hit for the Hero
+                    heroDamage += 3;
+                }
+                enemy.takeDamage(heroDamage);
 
-            if (Math.random() <= 0.3) {
-                setHealth(100);
-                System.out.println("Hero's health restored to 100.");
+                if (enemy.getHealth() <= 0) {
+                    // Hero wins
+                    health = Math.min(health + 100, 100 * level);
+                    if (random.nextDouble() <= 0.3) {
+                        // Hero receives a burst of energy
+                        health += 20;
+                    }
+                    experience += 50 * enemy.getLevel();
+                    if (experience >= 100 * level) {
+                        // Level up
+                        level++;
+                        health = health * level;
+                        int strengthIncrease = random.nextInt(4) + 3;
+                        strength = strength * level + strengthIncrease;
+                        experience = 0;
+                    }
+                    System.out.println("Hero wins!");
+                } else {
+                    System.out.println("Hero attacks! Enemy's health: " + enemy.getHealth());
+                }
+            } else {
+                // Enemy's turn to attack
+                int enemyDamage = enemy.calculateDamage();
+                if (random.nextDouble() <= 0.1) {
+                    // Critical hit for the Enemy
+                    enemyDamage += 3;
+                }
+                takeDamage(enemyDamage);
+
+                if (health <= 0) {
+                    // Enemy wins
+                    System.out.println("Enemy wins!");
+                } else {
+                    System.out.println("Enemy attacks! Hero's health: " + health);
+                }
             }
-            int experienceGained = 50 * enemy.level;
-            experience += experienceGained;
-            System.out.println("Hero gained " + experienceGained + " experience points.");
-        } else {
-            System.out.println("Hero attacked the enemy for " + damage + " damage.");
-            System.out.println("Enemy's health: " + enemy.health);
+
+            // End the battle after this round
+            break;
         }
+//        boolean isCriticalHit = Math.random() <= 0.1;
+//        int damage = strength + (isCriticalHit ? 3 : 0);
+//        enemy.health -= damage;
+//
+//        if (enemy.health <= 0) {
+//            enemy.health = 0;
+//            System.out.println("Hero defeated the enemy! ");
+//
+//            if (Math.random() <= 0.3) {
+//                setHealth(100);
+//                System.out.println("Hero's health restored to 100.");
+//            }
+//            int experienceGained = 50 * enemy.level;
+//            experience += experienceGained;
+//            System.out.println("Hero gained " + experienceGained + " experience points.");
+//        } else {
+//            System.out.println("Hero attacked the enemy for " + damage + " damage.");
+//            System.out.println("Enemy's health: " + enemy.health);
+//        }
     }
 
     public void avoid(Enemies enemy) {
@@ -91,14 +144,14 @@ public class Heroes {
         strength = Math.min(strength + (new Random().nextInt(6 - 3) + 3), maxStrength);
     }
 
-    public void takeDamage(int enemyDamage) {
+    public static void takeDamage(int enemyDamage) {
         health -= enemyDamage;
         if (health < 0) {
             health = 0; // Ensure health does not go below 0
         }
     }
 
-    public int calculateDamage() {
+    public static int calculateDamage() {
         Random random = new Random();
         int damage = strength;
 
@@ -110,7 +163,7 @@ public class Heroes {
         return damage;
     }
 
-    public void setExperience(int experience) {
+    public static void setExperience(int experience) {
         Heroes.experience = experience;
     }
 
